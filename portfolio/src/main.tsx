02cac26c5e88a4acb7100e670a2b507e17cebecc
EdiJunior88 <er.moura88@yahoo.com.br>
2023-07-20 17:16:03 -0300

    Update do portfolio

---
+++ portfolio/src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Home from "./Components/Home/Home"
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+)
