a8603bd1674c9d1f84906b1f70186c42dc31ad54
EdiJunior88 <er.moura88@yahoo.com.br>
2023-07-27 12:57:52 -0300

    - Adição de novas imagens de background

--- portfolio/src/Components/Avatar/Avatar.tsx
+++ portfolio/src/Components/Avatar/Avatar.tsx
@@ -2,7 +2,7 @@ import { InterfaceAvatar } from "../../Interface/Interface";
 
 const Avatar = ({ src, alt }: InterfaceAvatar) => {
   return (
-    <div className='background-button rounded-lg'>
+    <div className='background-button rounded-lg shadow-lg'>
       <img className='w-28 h-auto py-4' src={src} alt={alt} />
     </div>
   );
--- portfolio/src/Components/Button/Button.tsx
+++ portfolio/src/Components/Button/Button.tsx
@@ -3,7 +3,7 @@ import { InterfaceButton } from "../../Interface/Interface";
 const Button = ({ title, children }: InterfaceButton) => {
   return (
     <div>
-      <button className='w-56 rounded-lg bg-white p-2 border-solid border-2 background-button'>
+      <button className='w-56 rounded-lg bg-white p-2 border-zinc-200 border-solid border-2 background-button shadow-lg'>
         <div className='flex flex-col justify-center items-center gap-4'>
           {children}
           <p className='font-medium text-lg'>{title}</p>
