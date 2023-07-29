f404fcfbe64a9ec30e472919ee35a6bafc833fd7
EdiJunior88 <er.moura88@yahoo.com.br>
2023-07-28 23:38:28 -0300

    Update

--- portfolio/src/Pages/Home/Home.tsx
+++ portfolio/src/Pages/Home/Home.tsx
@@ -15,7 +15,7 @@ import Icons from "../../Components/Icons/Icons";
 const Home = () => {
   return (
     <div className='mx-auto p-4 w-full h-screen background-home'>
-      <div className='w-full h-full flex flex-col justify-around sm:justify-center items-center gap-10 sm:gap-32'>
+      <div className='w-full h-full flex flex-col justify-center items-center gap-8 sm:gap-20'>
         <Avatar
           src={Mii}
           alt='Mii avatar'
