1de6c78eb756018c5983a62e4b4bb84fe1ca3a90
EdiJunior88 <er.moura88@yahoo.com.br>
2023-08-01 13:53:28 -0300

    Update

--- portfolio/src/Components/Icons/Icons.tsx
+++ portfolio/src/Components/Icons/Icons.tsx
@@ -3,7 +3,11 @@ import { InterfaceIcons } from "../../Interface/Interface";
 const Icons = ({ children, title, link }: InterfaceIcons) => {
   return (
     <div>
-      <a className='flex flex-col' href={link} target='_blank'>
+      <a
+        className='flex flex-col'
+        href={link}
+        target='_blank'
+        rel='noopener noreferrer'>
         <button>
           <div className='icons flex flex-col justify-center items-center text-[10px] sm:text-base gap-2'>
             {children}
