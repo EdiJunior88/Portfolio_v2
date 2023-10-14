75b3537a5f2f25c0e334f3c8639640b1e6d53f5f
Edivaldo Junior <er.moura88@yahoo.com.br>
2023-10-14 20:27:33 -0300

    - Adição da biblioteca SplideJS para o Carousel Gallery

--- portfolio/src/main.ts
+++ portfolio/src/main.ts
@@ -8,6 +8,7 @@ import * as SiIcons from 'oh-vue-icons/icons/si'
 import * as FcIcons from 'oh-vue-icons/icons/fc'
 import * as BiIcons from 'oh-vue-icons/icons/bi'
 import * as OiIcons from 'oh-vue-icons/icons/oi'
+import VueSplide from '@splidejs/vue-splide'
 
 const Fc = Object.values({ ...SiIcons, ...FaIcons, ...FcIcons, ...BiIcons, ...OiIcons })
 addIcons(...Fc)
@@ -19,5 +20,6 @@ const app = createApp(App)
 app.component('v-icon', OhVueIcon)
 app.use(createPinia())
 app.use(router)
+app.use(VueSplide)
 
 app.mount('#app')
--- portfolio/src/page/ProjectPage.vue
+++ portfolio/src/page/ProjectPage.vue
@@ -1,7 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import '@splidejs/vue-splide/css'
+</script>
 
 <template>
-  <div>Página sobre os projetos</div>
+  <Splide :options="{ rewind: true }" aria-label="My Favorite Images">
+    <SplideSlide>
+      <img src="../assets/Image/aluratube-v2.webp" alt="Sample 1" />
+    </SplideSlide>
+    <SplideSlide>
+      <img src="../assets/Image/Consulta-Quadrinhos-v2-0.webp" alt="Sample 2" />
+    </SplideSlide>
+
+    <div class="splide__arrows" />
+  </Splide>
 </template>
 
 <style lang="css" scoped></style>
