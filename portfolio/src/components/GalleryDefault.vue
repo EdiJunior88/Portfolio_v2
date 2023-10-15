2656e124cd6513c5d9c8a7cf2a4464c633aca92a
Edivaldo Junior <er.moura88@yahoo.com.br>
2023-10-14 22:15:52 -0300

    - Estilizando o CSS do componente GalleryDefault - Adicionando novas imagens na galeria do componente ProjectPage

--- portfolio/src/components/GalleryDefault.vue
+++ portfolio/src/components/GalleryDefault.vue
@@ -1,20 +1,29 @@
 <script setup lang="ts">
+import { useThemeStore } from '@/theme/ThemeChange'
 import '@splidejs/vue-splide/css'
 
-defineProps(['images', 'href'])
+defineProps(['images'])
+
+const themeStore = useThemeStore()
 </script>
 
 <template>
-  <Splide :options="{ rewind: true }" aria-label="Galeria de Projetos">
-    <SplideSlide v-for="image in images" :key="image.src">
-      <a :href="href" target="_blank">
-        <div class="rounded-lg">
-          <slot></slot>
-        </div>
-      </a>
-      <img :src="image.src" :alt="image.alt" />
-    </SplideSlide>
-  </Splide>
+  <div :class="{ Switch: themeStore.Switch, WiiU: !themeStore.Switch }">
+    <div v-bind="$attrs">
+      <Splide :options="{ rewind: true }" aria-label="Galeria de Projetos">
+        <SplideSlide v-for="image in images" :key="image.src">
+          <a :href="image.href" target="_blank">
+            <div class="rounded-lg flex flex-col items-center p-3 gap-2">
+              <slot></slot>
+              <p class="font-medium">{{ image.title }}</p>
+            </div>
+          </a>
+          <img :src="image.src" :alt="image.alt" />
+        </SplideSlide>
+      </Splide>
+    </div>
+  </div>
+
   <div class="splide__arrows" />
 </template>
 
@@ -25,13 +34,28 @@ a {
   height: 100%;
   display: flex;
   justify-content: center;
-  align-items: center;
+  align-items: flex-end;
+  bottom: 39px;
 }
 
-div {
-  background: url(../assets//Background/SVG/background-mii-maker.svg);
+.WiiU div {
+  background: url(../assets/Background/background-button.webp);
   background-position: center center;
   background-repeat: no-repeat;
   background-size: cover;
+  z-index: 10;
+}
+
+.Switch div {
+  background: #fff;
+  z-index: 10;
+}
+
+.Switch p {
+  color: #41a5a3;
+}
+
+img {
+  opacity: 0.5;
 }
 </style>
--- portfolio/src/page/ProjectPage.vue
+++ portfolio/src/page/ProjectPage.vue
@@ -1,18 +1,55 @@
 <script setup lang="ts">
+import { useThemeStore } from '@/theme/ThemeChange'
 import GalleryDefault from '@/components/GalleryDefault.vue'
 import AluraTube from '@/assets/Image/aluratube-v2.webp'
 import ConsultaQuadrinhos from '@/assets/Image/Consulta-Quadrinhos-v2-0.webp'
+import GitHubFinder from '@/assets/Image/GitHub-Finder.webp'
+import NinjaList from '@/assets/Image/ninja-list.webp'
 
 let images = [
-  { src: AluraTube, alt: 'AluraTube' },
-  { src: ConsultaQuadrinhos, alt: 'ConsultaQuadrinhos' }
+  {
+    src: AluraTube,
+    alt: 'AluraTube',
+    href: 'https://aluratube-v2.netlify.app/',
+    title: 'AluraTube v2'
+  },
+  {
+    src: ConsultaQuadrinhos,
+    alt: 'ConsultaQuadrinhos',
+    href: 'https://consultaquadrinhosv2.netlify.app/',
+    title: 'Consulta Quadrinhos v2'
+  },
+  {
+    src: GitHubFinder,
+    alt: 'GitHubFinder',
+    href: 'https://githubfinder-edijunior88.netlify.app/',
+    title: 'GitHub Finder'
+  },
+  {
+    src: NinjaList,
+    alt: 'NinjaList',
+    href: 'https://githubfinder-edijunior88.netlify.app/',
+    title: 'Ninja List'
+  }
 ]
+
+const themeStore = useThemeStore()
 </script>
 
 <template>
-  <GalleryDefault :images="images">
-    <v-icon name="fa-github-square" scale="4" />
-  </GalleryDefault>
+  <div :class="{ Switch: themeStore.Switch, WiiU: !themeStore.Switch }">
+    <div v-if="themeStore.Switch">
+      <GalleryDefault :images="images">
+        <v-icon name="fa-globe-americas" scale="2.5" />
+      </GalleryDefault>
+    </div>
+
+    <div v-else>
+      <GalleryDefault :images="images">
+        <v-icon name="fc-globe" scale="2.5" />
+      </GalleryDefault>
+    </div>
+  </div>
 </template>
 
 <style lang="css" scoped></style>
