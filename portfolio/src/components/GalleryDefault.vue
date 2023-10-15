c260a22ab775e26442a713bf815f5c4c363e4162
Edivaldo Junior <er.moura88@yahoo.com.br>
2023-10-14 21:07:07 -0300

    - Criando um novo componente de Galeria Caroussel GalleryDefault - Ajustando o CSS do componente GalleryDefault

---
+++ portfolio/src/components/GalleryDefault.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import '@splidejs/vue-splide/css'
+
+defineProps(['images', 'href'])
+</script>
+
+<template>
+  <Splide :options="{ rewind: true }" aria-label="Galeria de Projetos">
+    <SplideSlide v-for="image in images" :key="image.src">
+      <a :href="href" target="_blank">
+        <div class="rounded-lg">
+          <slot></slot>
+        </div>
+      </a>
+      <img :src="image.src" :alt="image.alt" />
+    </SplideSlide>
+  </Splide>
+  <div class="splide__arrows" />
+</template>
+
+<style lang="css" scoped>
+a {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+div {
+  background: url(../assets//Background/SVG/background-mii-maker.svg);
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
--- portfolio/src/page/ProjectPage.vue
+++ portfolio/src/page/ProjectPage.vue
@@ -1,18 +1,18 @@
 <script setup lang="ts">
-import '@splidejs/vue-splide/css'
+import GalleryDefault from '@/components/GalleryDefault.vue'
+import AluraTube from '@/assets/Image/aluratube-v2.webp'
+import ConsultaQuadrinhos from '@/assets/Image/Consulta-Quadrinhos-v2-0.webp'
+
+let images = [
+  { src: AluraTube, alt: 'AluraTube' },
+  { src: ConsultaQuadrinhos, alt: 'ConsultaQuadrinhos' }
+]
 </script>
 
 <template>
-  <Splide :options="{ rewind: true }" aria-label="My Favorite Images">
-    <SplideSlide>
-      <img src="../assets/Image/aluratube-v2.webp" alt="Sample 1" />
-    </SplideSlide>
-    <SplideSlide>
-      <img src="../assets/Image/Consulta-Quadrinhos-v2-0.webp" alt="Sample 2" />
-    </SplideSlide>
-
-    <div class="splide__arrows" />
-  </Splide>
+  <GalleryDefault :images="images">
+    <v-icon name="fa-github-square" scale="4" />
+  </GalleryDefault>
 </template>
 
 <style lang="css" scoped></style>
