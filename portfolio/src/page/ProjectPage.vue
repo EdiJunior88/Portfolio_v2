66b935c780706752fab9ee6f5a9f9a9e5c9f8e35
Edivaldo Junior <er.moura88@yahoo.com.br>
2023-10-17 20:41:15 -0300

    - Ajustando a versão mobile dos seguintes componentes: ProjectPage - Estilizando o CSS do componente GalleryDefault - Adicionando novas imagens para a galeria de projetos

--- portfolio/src/page/ProjectPage.vue
+++ portfolio/src/page/ProjectPage.vue
@@ -1,10 +1,22 @@
 <script setup lang="ts">
 import { useThemeStore } from '@/theme/ThemeChange'
 import GalleryDefault from '@/components/GalleryDefault.vue'
-import AluraTube from '@/assets/Image/aluratube-v2.webp'
-import ConsultaQuadrinhos from '@/assets/Image/Consulta-Quadrinhos-v2-0.webp'
-import GitHubFinder from '@/assets/Image/GitHub-Finder.webp'
+import AluraTube from '@/assets/Image/aluratubev2.webp'
+import Bikcraft from '@/assets/Image/bikcraft.webp'
+import ConsultaQuadrinhos from '@/assets/Image/consulta-quadrinhos.webp'
+import ConsumoEnergia from '@/assets/Image/consumo-energia.webp'
+import ControleFinanceiro from '@/assets/Image/controle-financeiro.webp'
+import DogsOrigamid from '@/assets/Image/dogs-origamid.webp'
+import EnvioDinheiro from '@/assets/Image/envio-dinheiro.webp'
+import ExemploPortfolio from '@/assets/Image/exemplo-portfolio.webp'
+import ExemploPortfolioFreeCodeCamp from '@/assets/Image/exemplo-portfolio-freecodecamp.webp'
+import ExemploTailwindcss from '@/assets/Image/exemplo-tailwindcss.webp'
+import FavoritarRepositorios from '@/assets/Image/favoritar-repositorios.webp'
+import GithubFinder from '@/assets/Image/github-finder.webp'
+import ListaTarefas from '@/assets/Image/lista-tarefas.webp'
+import Musicdot from '@/assets/Image/musicdot.webp'
 import NinjaList from '@/assets/Image/ninja-list.webp'
+import TodoList from '@/assets/Image/todo-list.webp'
 
 let images = [
   {
@@ -14,23 +26,95 @@ let images = [
     title: 'AluraTube v2'
   },
   {
+    src: Bikcraft,
+    alt: 'Bikcraft',
+    href: 'https://bikcraft-edijunior88.netlify.app/',
+    title: 'Bikcraft'
+  },
+  {
     src: ConsultaQuadrinhos,
     alt: 'ConsultaQuadrinhos',
     href: 'https://consultaquadrinhosv2.netlify.app/',
     title: 'Consulta Quadrinhos v2'
   },
   {
-    src: GitHubFinder,
-    alt: 'GitHubFinder',
+    src: ConsumoEnergia,
+    alt: 'ConsumoEnergia',
+    href: 'https://edijunior88.github.io/Projeto_Verificar_KWh/',
+    title: 'Consumo Energia'
+  },
+  {
+    src: ControleFinanceiro,
+    alt: 'ControleFinanceiro',
+    href: 'https://edijunior88.github.io/NewTab_Academy_Projeto_Individual_JavaScript/',
+    title: 'Controle Financeiro'
+  },
+  {
+    src: DogsOrigamid,
+    alt: 'DogsOrigamid',
+    href: 'https://dogsorigamid-edijunior88.netlify.app/',
+    title: 'Dogs'
+  },
+  {
+    src: EnvioDinheiro,
+    alt: 'EnvioDinheiro',
+    href: 'https://newtab-academy-react.netlify.app/',
+    title: 'App Envio de Dinheiro'
+  },
+  {
+    src: ExemploPortfolio,
+    alt: 'ExemploPortfolio',
+    href: 'https://origamid-html-css-portfolio.netlify.app/',
+    title: 'Portfolio - Origamid'
+  },
+  {
+    src: ExemploPortfolioFreeCodeCamp,
+    alt: 'ExemploPortfolioFreeCodeCamp',
+    href: 'https://edijunior88.github.io/FreeCodeCamp_Projetos_do_Design_Responsivo/',
+    title: 'Portfolio - FreeCodeCamp'
+  },
+  {
+    src: ExemploTailwindcss,
+    alt: 'ExemploTailwindcss',
+    href: 'https://edijunior88.github.io/TailwindCSS_2_para_Iniciantes/',
+    title: 'Hotel - TailWindCSS'
+  },
+  {
+    src: FavoritarRepositorios,
+    alt: 'FavoritarRepositorios',
+    href: 'https://projeto-favoritar-repositorios.vercel.app/',
+    title: 'Favoritar Repositorios'
+  },
+  {
+    src: GithubFinder,
+    alt: 'GithubFinder',
     href: 'https://githubfinder-edijunior88.netlify.app/',
-    title: 'GitHub Finder'
+    title: 'Github Finder'
+  },
+  {
+    src: ListaTarefas,
+    alt: 'ListaTarefas',
+    href: 'https://edijunior88.github.io/Balta.io_Comecando_com_Angular/',
+    title: 'Lista de Tarefas'
+  },
+  {
+    src: Musicdot,
+    alt: 'Musicdot',
+    href: 'https://edijunior88.github.io/Caelum_HTML_CSS_JavaScript/',
+    title: 'Musicdot'
   },
   {
     src: NinjaList,
     alt: 'NinjaList',
-    href: 'https://githubfinder-edijunior88.netlify.app/',
+    href: 'https://hackathon-ninjalist.netlify.app/',
     title: 'Ninja List'
-  }
+  },
+  {
+    src: TodoList,
+    alt: 'TodoList',
+    href: 'https://baltaio-mytodolist.netlify.app/',
+    title: 'Todo List'
+  },
 ]
 
 const themeStore = useThemeStore()
